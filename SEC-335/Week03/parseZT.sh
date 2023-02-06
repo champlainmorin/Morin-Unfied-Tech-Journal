ffec0438b43d56686a3737e94cf062c190d5fcb1
champlainmorin <zachary.morin@mymail.champlain.edu>
2023-02-06 16:36:58 -0500

    Added Week03 stuff

--- SEC-335/Week03/parseZT.sh
+++ SEC-335/Week03/parseZT.sh
@@ -1,13 +1,25 @@
+# This script gets all the A records information from the zt.txt file
+
 part1=$(cat zt.txt | grep "	A	" | cut -d " " -f 1 | cut -f 1)
 #echo $part1
 part2=$(cat zt.txt | grep "	A	" | cut -f 4,5 | sed 's/A	//g')
 #echo $part2
 
 howmany() { echo $#; } # Gets the amount of items in the array.
-howmany $part1
-howmany $part2
+#howmany $part1
+part3=$(howmany $part2)
+#echo "$part3"
 
-#TODO Make a for loop to iterate over the length of part1
+argsPassed() { echo $@; }
+part4=$(argsPassed $part1)
 
-echo $part1 | cut -f 1 -d " "
+
+#echo $part1 | cut -f 1 -d " "
+
+for ip in $(seq 1 $part3); do 
+	test=$(echo $part1 | cut -d " " -f $ip)
+	test2=$(echo $part2 | cut -d " " -f $ip)
+	echo $test,$test2
+	#echo "hello"
+done
 
