a68816c50c5c4e79f5c7eb5118ba3926cb4f8893
champlainmorin <71420491+champlainmorin@users.noreply.github.com>
2023-02-07 15:07:06 -0500

    Fixed ps1 script again

--- SEC-335/Week03/dnsResolver.ps1
+++ SEC-335/Week03/dnsResolver.ps1
@@ -3,13 +3,10 @@ param($network, $dnsServer)
 for($i=1; $i -lt 255; $i++)
 {
     $ip=echo $network$i
-    $name=Resolve-DnsName -DnsOnly $ip -Server $dnsServer -ErrorAction Ignore | Select Name
-    $var=echo $name.Name
+    $name=Resolve-DnsName -DnsOnly $ip -Server $dnsServer -ErrorAction Ignore | Select NameHost
+    $var=echo $name.NameHost
     if ($var -is [string])
     {
-        if ($var.Contains("addr"))
-        {
-            echo "$ip $var"
-        }
+        echo "$ip $var"
     }
 }
