33d8f2417f0e868244d13836bc744e33b86f62f3
champlainmorin <71420491+champlainmorin@users.noreply.github.com>
2023-02-07 14:46:18 -0500

    dnsResolver.ps1 powershell script added

---
+++ SEC-335/Week03/dnsResolver.ps1
@@ -0,0 +1,10 @@
+﻿# Code by Zachary Morin, dnsResolver written in Powershell.
+
+param($network, $dnsServer)
+
+for($i=1; $i -lt 255; $i++)
+{
+    $ip=echo $network$i
+    $name=Resolve-DnsName -DnsOnly $ip -Server $dnsServer -ErrorAction Ignore | Select Name
+    echo $ip $name.Name
+}
