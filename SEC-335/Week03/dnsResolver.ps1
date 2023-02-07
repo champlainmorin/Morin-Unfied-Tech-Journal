45ff2cb5bb7c2f78884165f7b798ae5bbb11ff6d
champlainmorin <71420491+champlainmorin@users.noreply.github.com>
2023-02-07 15:02:15 -0500

    Updated dnsResolver to work properly

--- SEC-335/Week03/dnsResolver.ps1
+++ SEC-335/Week03/dnsResolver.ps1
@@ -1,10 +1,15 @@
-﻿# Code by Zachary Morin, dnsResolver written in Powershell.
-
 param($network, $dnsServer)
 
 for($i=1; $i -lt 255; $i++)
 {
     $ip=echo $network$i
     $name=Resolve-DnsName -DnsOnly $ip -Server $dnsServer -ErrorAction Ignore | Select Name
-    echo $ip $name.Name
+    $var=echo $name.Name
+    if ($var -is [string])
+    {
+        if ($var.Contains("addr"))
+        {
+            echo "$ip $var"
+        }
+    }
 }
