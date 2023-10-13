4f9f05718799076d1c208e1699c57c2bda8752bf
champlainmorin <71420491+champlainmorin@users.noreply.github.com>
2023-10-12 22:37:10 -0400

    Create grepper.php

---
+++ SEC-480/grepper.php
@@ -0,0 +1,19 @@
+<form id="logform" method="post">
+<div>Search Term: <input type="text" name="search"><div>
+</select>
+<div class="full-width"></br>
+  <button type="submit">Search</button>
+</div>
+</form>
+<?php
+if(isset($_POST['search'])) {
+  $searchterm=$_POST['search'];
+  echo "<div>";
+  echo "<h1>Searchterm:" . $searchterm . "</h1>";
+  echo "</div>";
+
+  echo "<pre>";
+  passthru("cat /usr/share/wordlists/rockyou.txt | grep " . $searchterm);
+  echo "</pre>";
+}
+?>
